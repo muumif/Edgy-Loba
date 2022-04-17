8d3e3c3b1ae1181c85acb9be04b0d6d317497318
muumi <chrisrobin.soon@gmail.com>
2022-04-17 12:11:48 +0300

    Step size 200 graph

--- main.js
+++ main.js
@@ -84,14 +84,14 @@ async function fetchUser(id, callback){
 };
 
 function makeChart(_labels = [],_data = []){
-    const chart = `https://image-charts.com/chart.js/2.8.0?bkg=rgb(54,57,63)&c={type:'line',data:{labels:[${_labels.map(function(ele){return "'" + ele + "'"})}],datasets:[{backgroundColor:'rgba(44,47,51,0)',borderColor:'rgb(277,166,0)',data:[${_data}],label:'RP'}]},options:{scales:{yAxes:[{ticks:{stepSize: 100}}]}}}`;
+    const chart = `https://image-charts.com/chart.js/2.8.0?bkg=rgb(54,57,63)&c={type:'line',data:{labels:[${_labels.map(function(ele){return "'" + ele + "'"})}],datasets:[{backgroundColor:'rgba(44,47,51,0)',borderColor:'rgb(277,166,0)',data:[${_data}],label:'RP'}]},options:{scales:{yAxes:[{ticks:{stepSize: 200}}]}}}`;
     return encodeURI(chart);
 };
 
 client.once("ready", () => {
     console.log("Edgy Loba is now online!");
     client.user.setPresence({ activity: { name: ">help", type: "LISTENING" }, status: "online" });
-})
+});
 
 client.on("message", async message => {
 
