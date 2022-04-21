f818afb55677f4b228e7a17067c5be2f6f6c62ba
muumi <chrisrobin.soon@gmail.com>
2022-04-21 22:18:54 +0300

    Help command to new rework.

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const { set, getDatabase, ref, get, child } = require('firebase/database');
 const { makeMapEmbed } = require('./commands/apexMisc/map');
 const { makeStatusEmbed } = require('./commands/apexMisc/status');
 const { makeTopEmbed } = require('./commands/userStats/tops');
+const { makeHelpEmbed } = require('./commands/help');
 
 const client = new Discord.Client();
 const prefix = process.env.PREFIX;
@@ -134,44 +135,9 @@ client.on("message", async message => {
     const command = args.shift().toLowerCase();
 
     if (command === "help"){
-        
-        const embed = new Discord.MessageEmbed()
-        .setTitle("Help/Commands")
-        .setColor("#e3a600")
-        .addFields(
-            {
-                name: "__>link [Apex IGN] [PC/X1/PS4]__",
-                value: "Link your discord account to your Apex user. Doing this allows you to see history graphs collaborate in the leaderboard and much more.",
-                inline: true
-            },
-            {
-                name: "__>unlink__",
-                value: "Unlink your discord account from you Apex user.",
-                inline: true
-            },
-            {
-                name: "__>stats [Optional: Apex IGN] [Optional: PC/X1/PS4]__",
-                value: "Shows Apex stats. If you have a linked account you dont have to insert your Apex IGN to check your own stats.",
-                inline: true
-            }, 
-            {
-                name: "__>top__",
-                value: "Shows the current leaderboard for this server.",
-                inline: true
-            },
-            {
-                name: "__>map__",
-                value: "Shows the current and next maps for Apex, Battle Royale and Arenas mode also the time remaining till next map.",
-                inline: true
-            },
-            {
-                name: "__>status__",
-                value: "Shows the server status of Lobby/Matchmaking servers by region.",
-                inline: true
-            }
-        );
-
-        message.channel.send({embed});
+        makeHelpEmbed().then(result => {
+            message.channel.send(result);
+        });
     }
 
     if (command === "link"){ // Find a way to confirm that it is their account
