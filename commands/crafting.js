efa3a8533d227c99dcead88b0771e1a847e40574
muumi <chrisrobin.soon@gmail.com>
2022-06-06 16:09:13 +0300

    crafting half

---
+++ commands/crafting.js
@@ -0,0 +1,72 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const axios = require("axios");
+require("dotenv").config();
+const { MessageEmbed } = require("discord.js");
+const { logger } = require("../moduels/logger");
+
+async function getData(guildID, discordID) {
+	const URI = `${process.env.ALS_ENDPOINT}/crafting?auth=${process.env.ALS_TOKEN}`;
+	return await axios.get(encodeURI(URI))
+		.then(function(response) {
+			logger.info("API fetched crafting data!", { command: "crafting", guildID: guildID, discordID: discordID });
+			return response;
+		}).catch(error => {
+			const embed = new MessageEmbed()
+				.setColor("#e3a600");
+			switch (error.response.status) {
+			case 400:
+				embed.setTitle("Something went wrong.");
+				embed.setDescription("Try again in a few minutes.");
+				logger.error(new Error(error), { command: "crafting", guildID: guildID, discordID: discordID });
+				return Promise.reject(embed);
+			case 403:
+				embed.setTitle("Unauthorized / Unknown API key.");
+				embed.setDescription("The bot might be worked on at this moment. If this continues to happen report it with /bug.");
+				logger.error(new Error(error), { command: "crafting", guildID: guildID, discordID: discordID });
+				return Promise.reject(embed);
+			case 404:
+				embed.setTitle("Player could not be found.");
+				embed.setDescription("If this continues to happen check that you are using your origin username or report the bug with /bug.");
+				logger.error(new Error(error), { command: "crafting", guildID: guildID, discordID: discordID });
+				return Promise.reject(embed);
+			case 405:
+				embed.setTitle("External API error.");
+				embed.setDescription("Try again in a few seconds.");
+				logger.error(new Error(error), { command: "crafting", guildID: guildID, discordID: discordID });
+				return Promise.reject(embed);
+			case 410:
+				embed.setTitle("Unknown platform provided.");
+				embed.setDescription("If this continues to happen report it as a bug with /bug");
+				logger.error(new Error(error), { command: "crafting", guildID: guildID, discordID: discordID });
+				return Promise.reject(embed);
+			case 429:
+				embed.setTitle("API Rate limit reached.");
+				embed.setDescription("Try again in a few seconds.");
+				logger.error(new Error(error), { command: "crafting", guildID: guildID, discordID: discordID });
+				return Promise.reject(embed);
+			case 500:
+				embed.setTitle("API Internal error.");
+				logger.error(new Error(error), { command: "crafting", guildID: guildID, discordID: discordID });
+				return Promise.reject(embed);
+			}
+		});
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("help")
+		.setDescription("Show all commands!")
+		.addStringOption(option =>
+			option.setName("category")
+				.setDescription("The help category!")
+				.setRequired(true)
+				.addChoices(
+					{ name: "misc", value: "help_misc" },
+					{ name: "stats", value: "help_stats" },
+					{ name: "admin", value: "help_admin" },
+				)),
+	async execute(interaction) {
+		if (!interaction.isCommand()) return;
+
+	},
+};
