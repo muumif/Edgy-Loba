7d15eecf0d6814caf6703c04c4e14d6b29b2429a
muumi <chrisrobin.soon@gmail.com>
2022-06-17 23:45:51 +0300

    Use JSONBigInt so values dont get rounded

--- moduels/getUID.js
+++ moduels/getUID.js
@@ -1,20 +1,22 @@
 const axios = require("axios");
 require("dotenv").config();
 const { logger } = require("./logger");
+const JSONBigInt = require("json-bigint")({ "storeAsString": true });
+
 
 async function getUserUID(IGN, platform, guildID, discordID) {
 	try {
-		const response = await axios.get(encodeURI(`${process.env.ALS_ENDPOINT}/nametouid?auth=${process.env.ALS_TOKEN}&player=${IGN}&platform=${platform}`));
+		const response = await axios.get(encodeURI(`${process.env.ALS_ENDPOINT}/nametouid?auth=${process.env.ALS_TOKEN}&player=${IGN}&platform=${platform}`), { transformResponse: [data => data] });
 		if (response.data.Error != undefined) {
 			logger.error(new Error(response.data.Error), { module: "getUID", guildID: guildID, discordID: discordID, data: response.data, IGN: IGN, platform: platform });
 			return Promise.reject(response.data.Error);
 		}
 		if (platform == "PC") {
 			logger.info("ALS API data fetched user PC!", { module: "getUID", guildID: guildID, discordID: discordID, data: response.data, IGN: IGN, platform: platform });
-			return response.data.uid;
+			return JSONBigInt.parse(response.data).uid;
 		}
 		logger.info("API data fetched user console!", { module: "getUID", guildID: guildID, discordID: discordID, data: response.data, IGN: IGN, platform: platform });
-		return response.data.result;
+		return JSONBigInt.parse(response.data).result;
 	}
 	catch (error) {
 		//Cant do anything until new headears from the API
