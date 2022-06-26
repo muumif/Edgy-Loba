bb3e2ca9adc51ea34995be3dec22120fa06efee2
muumi <chrisrobin.soon@gmail.com>
2022-06-26 17:35:13 +0300

    Global command deployer (Closes #106)

--- misc/internal/deploy.js
+++ misc/internal/deploy.js
@@ -1,13 +1,12 @@
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const fs = require("node:fs");
+const fs = require("fs");
+const { logger } = require("./logger");
 
 const commands = [];
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
-// Place your client and guild ids here
 const clientId = "967426810528825368";
-const guildId = "812033742419001355";
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -18,16 +17,16 @@ const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
 	try {
-		console.log("Started refreshing application (/) commands.");
+		logger.info("Global command refreshing started!", { module: "deploy" });
 
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
-		console.log("Successfully reloaded application (/) commands.");
+		logger.info("Global command refreshing was successful!", { module: "deploy" });
 	}
 	catch (error) {
-		console.error(error);
+		logger.error(new Error(error), { module: "deploy" });
 	}
 })();
