69127053261c3b0fe4c6ed9c2469dd1a34a7a637
muumi <chrisrobin.soon@gmail.com>
2022-07-14 15:57:07 +0300

    Rework crafting command (#120)

--- commands/misc/crafting.js
+++ commands/misc/crafting.js
@@ -23,21 +23,59 @@ module.exports = {
 			const craftingData = await axios.get(encodeURI(`${process.env.ALS_ENDPOINT}/crafting?auth=${process.env.ALS_TOKEN}`));
 			const embed = new MessageEmbed()
 				.setTitle("Crafting Rotation")
-				.addFields(
-					{
-						name: "__Daily__",
-						value: "**1. " + craftingData.data[0].bundleContent[0].itemType.rarity + " " + craftingData.data[0].bundleContent[0].itemType.name + "**\nCost: " + craftingData.data[0].bundleContent[0].cost + " CM\n**2. " + craftingData.data[0].bundleContent[1].itemType.rarity + " " + craftingData.data[0].bundleContent[1].itemType.name + "**\nCost: " + craftingData.data[0].bundleContent[1].cost + " CM",
-						inline: true,
-					},
-					{
-						name: "__Weekly__",
-						value: "**1. " + craftingData.data[1].bundleContent[0].itemType.rarity + " " + craftingData.data[1].bundleContent[0].itemType.name + "**\nCost: " + craftingData.data[1].bundleContent[0].cost + " CM\n**2. " + craftingData.data[1].bundleContent[1].itemType.rarity + " " + craftingData.data[1].bundleContent[1].itemType.name + "**\nCost: " + craftingData.data[1].bundleContent[1].cost + " CM",
-						inline: true,
-					},
-				)
 				.setColor("#e3a600")
 				.setFooter({ text: "Bugs can be reported with /bug", iconURL: "https://cdn.discordapp.com/avatars/719542118955090011/82a82af55e896972d1a6875ff129f2f7.png?size=256" })
 				.setTimestamp();
+
+			const timer = (endTime) => {
+				const milliSeconds = new Date(endTime * 1000) - new Date().getTime();
+				const days = Math.floor(milliSeconds / 86400000);
+				const hours = Math.floor(milliSeconds / 3600000) - (days * 24);
+				const minutes = Math.floor(milliSeconds / 60000) - ((days * 24) * 60) - (hours * 60);
+				const seconds = Math.floor(milliSeconds / 1000) - (((days * 24) * 60) * 60) - ((hours * 60) * 60) - (minutes * 60);
+				return {
+					seconds: seconds,
+					minutes: minutes,
+					hours: hours,
+					days: days,
+				};
+
+			};
+
+			for (let i = 0; i < craftingData.data.length; i++) {
+				for (let j = 0; j < craftingData.data[i].bundleContent.length; j++) {
+
+					let color;
+					switch (craftingData.data[i].bundleContent[j].itemType.rarity) {
+					case "Common":
+						color = "\u001b[0;36m";
+						break;
+					case "Rare":
+						color = "\u001b[0;34m";
+						break;
+					case "Epic":
+						color = "\u001b[0;35m";
+						break;
+					case "Legendary":
+						color = "\u001b[0;33m";
+						break;
+					}
+
+					if (craftingData.data[i].bundleType != "permanent") {
+						embed.addField(String(craftingData.data[i].bundleType).charAt(0).toUpperCase() + String(craftingData.data[i].bundleType).slice(1), `${"```ansi"}\n\u001b[0;37mItem: ${color}${String(craftingData.data[i].bundleContent[j].itemType.name).replace("_", " ").replace("_", " ").charAt(0).toUpperCase() + String(craftingData.data[i].bundleContent[j].itemType.name).replace("_", " ").replace("_", " ").slice(1) }\n\u001b[0;37mCost: \u001b[0;33m${craftingData.data[i].bundleContent[j].cost}\n\u001b[0;37mEnds In: \n\u001b[0;33m${timer(craftingData.data[i].end).days}\u001b[0;37mD - \u001b[0;33m${timer(craftingData.data[i].end).hours}\u001b[0;37mH\n\u001b[0;33m${timer(craftingData.data[i].end).minutes}\u001b[0;37mmin - \u001b[0;33m${timer(craftingData.data[i].end).seconds}\u001b[0;37msec${"```"}`, true);
+					}
+					else {
+						if (craftingData.data[i].bundleContent[j].itemType.name != "ammo") {
+							embed.addField(String(craftingData.data[i].bundleType).charAt(0).toUpperCase() + String(craftingData.data[i].bundleType).slice(1), `${"```ansi"}\n\u001b[0;37mItem: ${color}${String(craftingData.data[i].bundleContent[j].itemType.name).replace("_", " ").replace("_", " ").charAt(0).toUpperCase() + String(craftingData.data[i].bundleContent[j].itemType.name).replace("_", " ").replace("_", " ").slice(1)}\n\u001b[0;37mCost: \u001b[0;33m${craftingData.data[i].bundleContent[j].cost}\n\u001b[0;37m${"```"}`, true);
+
+						}
+
+					}
+
+				}
+			}
+
+
 			return await interaction.editReply({ embeds: [embed] });
 		}
 		catch (error) {
