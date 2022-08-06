89f120ef310bcc138dd9cd5c58019869d1a1c630
muumi <chrisrobin.soon@gmail.com>
2022-08-06 23:47:49 +0300

    Logger file

---
+++ src/internal/logger.ts
@@ -0,0 +1,29 @@
+import  { Logger, format, createLogger, transports } from  'winston';
+
+const customFormat = format.printf(({ level, message, timestamp }) => {
+      return `${timestamp} | ${level} | ${message}`;
+});
+
+export const logger: Logger = createLogger({
+      level: "info",
+      format: format.combine(
+		format.timestamp(),
+		format.errors({ stack: true }),
+		format.splat(),
+		format.simple(),
+		format.json(),
+      ),
+
+      transports: [
+      		new transports.Console({ // Print logs to the console with a customFormat
+			format: format.combine(
+				format.timestamp({
+					format: "HH:mm:ss",
+				}),
+				format.colorize(),
+				customFormat,
+			),
+		}),
+		new transports.File({ filename: "./logs/combined.log" }), // Save logs to a file combined.log
+	],
+})
