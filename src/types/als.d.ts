3182bb8322aea4b8345a5328bfb545eed7a4e6a9
muumi <chrisrobin.soon@gmail.com>
2022-08-08 19:55:16 +0300

    feat: add next command

--- src/types/als.d.ts
+++ src/types/als.d.ts
@@ -45,3 +45,114 @@ export interface NewsData {
       img: string,
       short_desc: string,
 }
+
+export interface MapData {
+	battle_royale: {
+		current: {
+			start: number,
+			end: number,
+			readableDate_start: string,
+			readableDate_end: string,
+			map: string,
+			code: string,
+			DurationInSecs: number,
+			DurationInMinutes: number,
+			asset: string,
+			remainingSecs: number,
+			remainingMins: number,
+			remainingTimer: string
+		},
+		next: {
+			start: number,
+			end: number,
+			readableDate_start: string,
+			readableDate_end: string,
+			map: string,
+			code: string,
+			DurationInSecs: number,
+			DurationInMinutes: number,
+			asset: string,
+		}
+	},
+	arenas: {
+		current: {
+			start: number,
+			end: number,
+			readableDate_start: string,
+			readableDate_end: string,
+			map: string,
+			code: string,
+			DurationInSecs: number,
+			DurationInMinutes: number,
+			asset: string,
+			remainingSecs: number,
+			remainingMins: number,
+			remainingTimer: string
+		},
+		next: {
+			start: number,
+			end: number,
+			readableDate_start: string,
+			readableDate_end: string,
+			map: string,
+			code: string,
+			DurationInSecs: number,
+			DurationInMinutes: number,
+			asset: string
+		}
+	},
+	ranked: {
+		current: {
+			start: number,
+			end: number,
+			readableDate_start: string,
+			readableDate_end: string,
+			map: string,
+			code: string,
+			DurationInSecs: number,
+			DurationInMinutes: number,
+			asset: string,
+			remainingSecs: number,
+			remainingMins: number,
+			remainingTimer: string
+		},
+		next: {
+			start: number,
+			end: number,
+			readableDate_start: string,
+			readableDate_end: string,
+			map: string,
+			code: string,
+			DurationInSecs: number,
+			DurationInMinutes: number,
+			asset: string
+		}
+	},
+	arenasRanked: {
+		current: {
+			start: number,
+			end: number,
+			readableDate_start: string,
+			readableDate_end: string,
+			map: string,
+			code: string,
+			DurationInSecs: number,
+			DurationInMinutes: number,
+			asset: string,
+			remainingSecs: 1483,
+			remainingMins: 25,
+			remainingTimer: string
+		},
+		next: {
+			start: number,
+			end: number,
+			readableDate_start: string,
+			readableDate_end: string,
+			map: string,
+			code: string,
+			DurationInSecs: number,
+			DurationInMinutes: number,
+			asset:string
+		}
+	},
+}
