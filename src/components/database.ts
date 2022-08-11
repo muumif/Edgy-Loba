1d9e20ca782759ede89b71bb1eeb1c71ef5cebd0
muumi <chrisrobin.soon@gmail.com>
2022-08-11 13:36:47 +0300

    feat: logging to the database manager file

--- src/components/database.ts
+++ src/components/database.ts
@@ -1,13 +1,22 @@
 import { Guild, Snowflake } from "discord.js";
 import { MongoClient } from "mongodb";
 import { HistoryDocument, UserDocument } from "../types/mongo";
+import { filename } from "./const";
+import { logger } from "./logger";
 
 const server = `mongodb://${process.env.MONGO_CONNECTION}/?authMechanism=DEFAULT`;
-
 const DBClient = new MongoClient(server);
-const usersCollection = DBClient.db("EdgyLoba").collection("users");
-const historyCollection = DBClient.db("EdgyLoba").collection("userHistory");
-const guildCollection = DBClient.db("EdgyLoba").collection("guilds");
+
+let usersCollection = DBClient.db("EdgyLoba").collection("users");
+let historyCollection = DBClient.db("EdgyLoba").collection("userHistory");
+let guildCollection = DBClient.db("EdgyLoba").collection("guilds");
+
+if (process.env.NODE_ENV == "development") {
+      usersCollection = DBClient.db("EdgyLobaDEV").collection("users");
+      historyCollection = DBClient.db("EdgyLobaDEV").collection("userHistory");
+      guildCollection = DBClient.db("EdgyLobaDEV").collection("guilds");
+}
+
 //TODO: Logging for calls
 export class User {
       discordId: Snowflake;
@@ -22,8 +31,9 @@ export class User {
 
                   const user = await usersCollection.findOne({ discordId: this.discordId.toString() }) as UserDocument | null;
 
-                  if (user == null) return Promise.reject("User not found!");
+                  if (user == null) return Promise.resolve("User not found!");
 
+                  logger.info("Fetched a user from the DB!", { discordId: this.discordId, file: filename(__filename) });
                   return Promise.resolve(user);
             }
             catch (error) {
@@ -42,6 +52,7 @@ export class User {
 
                   if (history.length == 0) return Promise.reject("No history data was found!");
 
+                  logger.info("Fetched history from the DB!", { discordId: this.discordId, file: filename(__filename) });
                   return Promise.resolve(history);
             }
             catch (error) {
@@ -52,10 +63,10 @@ export class User {
             }
       }
 
-      public async addUser(originId: string, RP: number, AP: number, platform: string, serverId: Snowflake) {
+      public async addUser(originId: string, RP: number, AP: number, platform: string, serverId: Snowflake | undefined) {
             try {
                   await DBClient.connect();
-
+                  const id = this.discordId;
                   return await usersCollection.insertOne({
                         discordId: this.discordId,
                         originId: originId,
@@ -64,7 +75,10 @@ export class User {
                         platform: platform,
                         servers: [serverId],
                   })
-                        .then(function() {return Promise.resolve("User data inserted");});
+                        .then(function() {
+                              logger.info("Added a user to the DB!", { serverId: serverId, discordId: id, file: filename(__filename) });
+                              return Promise.resolve("User data inserted");
+                        });
             }
             catch (error) {
                   return Promise.reject(error);
@@ -77,14 +91,17 @@ export class User {
       public async addHistory(RP: number, AP: number) {
             try {
                   await DBClient.connect();
-
+                  const id = this.discordId;
                   return await historyCollection.insertOne({
                         discordId: this.discordId,
                         date: new Date(),
                         RP: RP,
                         AP: AP,
                   })
-                        .then(function() {return Promise.resolve("History data inserted");});
+                        .then(function() {
+                              logger.info("Added history data to the DB!", { discordId: id, file: filename(__filename) });
+                              return Promise.resolve("History data inserted");
+                        });
             }
             catch (error) {
                   return Promise.reject(error);
@@ -97,9 +114,12 @@ export class User {
       public async addServer(serverId: Snowflake) {
             try {
                   await DBClient.connect();
-
+                  const id = this.discordId;
                   return await usersCollection.updateOne({ discordId: this.discordId }, { $push: { servers: serverId } })
-                        .then(function() {return Promise.resolve("Server data inserted");});
+                        .then(function() {
+                              logger.info("Updated a users servers in the DB!", { serverId: serverId, discordId: id, file: filename(__filename) });
+                              return Promise.resolve("Server data inserted");
+                        });
 
             }
             catch (error) {
@@ -113,9 +133,12 @@ export class User {
       public async updateRP(RP: number) {
             try {
                   await DBClient.connect();
-
+                  const id = this.discordId;
                   return await usersCollection.updateOne({ discordId: this.discordId }, { $set:{ RP: RP } })
-                        .then(function() {return Promise.resolve("Updated RP");});
+                        .then(function() {
+                              logger.info("Updated a users RP in the DB!", { discordId: id, file: filename(__filename) });
+                              return Promise.resolve("Updated RP");
+                        });
             }
             catch (error) {
                   return Promise.reject(error);
@@ -128,9 +151,12 @@ export class User {
       public async updateAP(AP: number) {
             try {
                   await DBClient.connect();
-
+                  const id = this.discordId;
                   return await usersCollection.updateOne({ discordId: this.discordId }, { $set:{ AP: AP } })
-                        .then(function() {return Promise.resolve("Updated AP");});
+                        .then(function() {
+                              logger.info("Updated a users RP in the DB!", { discordId: id, file: filename(__filename) });
+                              return Promise.resolve("Updated AP");
+                        });
             }
             catch (error) {
                   return Promise.reject(error);
@@ -143,9 +169,12 @@ export class User {
       public async deleteUser() {
             try {
                   await DBClient.connect();
-
+                  const id = this.discordId;
                   return await usersCollection.deleteOne({ discordId: this.discordId })
-                        .then(function() {return Promise.resolve("Deleted user");});
+                        .then(function() {
+                              logger.info("Deleted a user from the DB!", { discordId: id, file: filename(__filename) });
+                              return Promise.resolve("Deleted user");
+                        });
             }
             catch (error) {
                   return Promise.reject(error);
@@ -165,12 +194,15 @@ export class Server {
       public async addServer() {
             try {
                   await DBClient.connect();
-
+                  const id = this.guild.id;
                   return await guildCollection.insertOne({
                         serverId: this.guild.id,
                         name: this.guild.name,
                   })
-                        .then(function() {return Promise.resolve("Inserted server");});
+                        .then(function() {
+                              logger.info("Added a server to the DB!", { serverId: id, file: filename(__filename) });
+                              return Promise.resolve("Inserted server");
+                        });
 
             }
             catch (error) {
@@ -185,6 +217,7 @@ export class Server {
             try {
                   await DBClient.connect();
 
+                  logger.info("Deleted a server from the DB!", { serverId: this.guild.id, file: filename(__filename) });
                   return await guildCollection.deleteOne({ serverId: this.guild.id });
             }
             catch (error) {
