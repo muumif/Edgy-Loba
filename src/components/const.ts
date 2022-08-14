ba468bc2f78505b6aceef2cac4ef2db3d82b8b29
muumi <chrisrobin.soon@gmail.com>
2022-08-14 14:45:30 +0300

    feat: add used emojis to const

--- src/components/const.ts
+++ src/components/const.ts
@@ -1,6 +1,6 @@
 //Constants that are required in multiple files and when changed need to be same everywhere
 
-import { APIEmbedField } from "discord.js";
+import { APIEmbedField, CommandInteraction, GuildEmoji } from "discord.js";
 
 export function profilePic(size: number): string {
       return `https://cdn.discordapp.com/avatars/719542118955090011/82a82af55e896972d1a6875ff129f2f7.png?size=${size}`;
@@ -20,3 +20,14 @@ export function filename(filename: string) {
 }
 
 export const chartBackgroundColor = "#36393f";
+
+export function emojis(interaction: CommandInteraction) {
+      const PCEmoji = interaction.client.emojis.cache.get("987422520363868251") as GuildEmoji;
+      const PSEmoji = interaction.client.emojis.cache.get("987422521680855100") as GuildEmoji;
+      const XboxEmoji = interaction.client.emojis.cache.get("987422524654641252") as GuildEmoji;
+      const OnlineEmoji = interaction.client.emojis.cache.get("987434490525794435") as GuildEmoji;
+      const IdleEmoji = interaction.client.emojis.cache.get("987439560856334356") as GuildEmoji;
+      const OfflineEmoji = interaction.client.emojis.cache.get("987434491951841371") as GuildEmoji;
+
+      return [PCEmoji, PSEmoji, XboxEmoji, OnlineEmoji, IdleEmoji, OfflineEmoji] as GuildEmoji[];
+}
