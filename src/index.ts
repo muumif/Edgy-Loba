27110d07674014709d2fc920ba08486f817be5db
muumi <chrisrobin.soon@gmail.com>
2022-08-16 13:40:04 +0300

    feat: change presence every 10 minutes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { filename } from "./components/const";
 import { DBGlobal } from "./components/database";
 import path from "path";
 import "./components/scheduler";
+import moment from "moment";
 
 export const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -33,8 +34,24 @@ if (!existsSync(tempFolder)) {
 client.once("ready", () => {
       logger.info("▬▬ι═══════ﺤ Edgy Loba is now online -═══════ι▬▬", { file: filename(__filename) });
       logger.info(`Hostname: ${hostname} | Environment: ${process.env.NODE_ENV} | Version: ${process.env.npm_package_version} | OS: ${type} ${version}`, { file: filename(__filename) });
+
+      const duration = moment(client.uptime as number).format("H [hrs], m [mins]");
+      const presences = [
+            { type: 3, name: `${client.guilds.cache.size} servers!` },
+            { type: 1, name: `${client.guilds.cache.size} servers!` },
+            { type: 2, name: `${client.guilds.cache.size} servers!` },
+            { type: 3, name: `uptime ${duration}` },
+            { type: 2, name: "/help" },
+            { type: 2, name: "/about" },
+            { type: 1, name: `version ${process.env.npm_package_version}` },
+      ];
+
       if (process.env.NODE_ENV == "production") {
             client.user?.setPresence({ activities: [{ name: `${client.guilds.cache.size} servers!`, type: ActivityType.Playing }], status: "online" });
+            setInterval(() => {
+                  const presence = presences[Math.floor(Math.random() * presences.length)];
+                  client.user?.setPresence({ activities: [{ name: `${presence.name}`, type: presence.type }], status: "online" });
+            }, 600000);
       }
       else {
             client.user?.setPresence({ activities: [{ name: "Internal build!" }], status: "dnd" });
@@ -54,8 +71,9 @@ client.on("interactionCreate", async interaction => {
                         return;
                   }
                   const deferredReply = await interaction.deferReply({ fetchReply: true });
+                  const dateNow = Date.now();
                   await command.execute(interaction);
-                  logger.info(`[${interaction.user.username}] used [/${interaction.commandName}] in [${interaction.guild?.name}]. Bot response time: ${deferredReply.createdTimestamp - interaction.createdTimestamp}ms`, { command: interaction.commandName, discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename), responseTime: deferredReply.createdTimestamp - interaction.createdTimestamp });
+                  logger.info(`[${interaction.user.username}] used [/${interaction.commandName}] in [${interaction.guild?.name}]. Bot response time: ${interaction.createdTimestamp - dateNow}ms`, { command: interaction.commandName, discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename), responseTime: interaction.createdTimestamp - dateNow });
             }
             catch (error) {
                   await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
