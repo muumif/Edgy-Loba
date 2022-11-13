77ee8418c7ff0313bd842545d7d58d5356a38af2
gitTanzj <kalle.riit@gmail.com>
2022-11-13 22:09:51 +0200

    feat; reworked help command

--- src/commands/main/help.ts
+++ src/commands/main/help.ts
@@ -1,6 +1,7 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, SlashCommandBuilder, APIEmbedField } from "discord.js";
 import { profilePic, linksField } from "../../components/const";
 import { embed } from "../../components/embeds";
+import { getCommands } from "../../components/commands";
 
 module.exports = {
       data: new SlashCommandBuilder()
@@ -24,46 +25,7 @@ module.exports = {
                         .setTitle("Help Miscellaneous")
                         .setThumbnail(profilePic(512))
                         .addFields(
-                              {
-                                    name: "`/about`",
-                                    value: "See almost everything about the bot.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/crafting`",
-                                    value: "See the current items that can be crafted at the replicator.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/map`",
-                                    value: "See each map currently in the rotation and the times when they end.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/news`",
-                                    value: "See the latest news from EA news feed about Apex Legends.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/predator`",
-                                    value: "See how much RP/AP is needed to reach Predator on all platforms.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/status`",
-                                    value: "See the EA servers status and the response time.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/distribution`",
-                                    value: "See the rank distribution.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/next`",
-                                    value: "See when a new split or season starts.",
-                                    inline: false,
-                              },
+                              getCommands("misc"),
                               linksField("Links", false),
                         );
                   return await interaction.editReply({ embeds: [helpEmbed] });
@@ -73,36 +35,7 @@ module.exports = {
                         .setTitle("Help Stats")
                         .setThumbnail(profilePic(512))
                         .addFields(
-                              {
-                                    name: "`/stats`",
-                                    value: "See a given users stats. Must use Origin username.",
-                                    inline: false,
-                              },
-                              {
-                                    name:"`/me`",
-                                    value: "See your own stats.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/link`",
-                                    value: "Link a Discord account to an Origin user.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/unlink`",
-                                    value: "Unlink a Discord account from an Origin user.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/top`",
-                                    value: "See the top 10 users in the server.",
-                                    inline: false,
-                              },
-                              {
-                                    name: "`/gtop`",
-                                    value: "See the top 3 global users on the bot.",
-                                    inline: false,
-                              },
+                              getCommands("stats"),
                               linksField("Links", false),
                         );
                   return await interaction.editReply({ embeds: [helpEmbed] });
