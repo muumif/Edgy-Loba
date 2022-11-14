8d1c70f2d94fc97ec03358c8ee497446712809b1
muumi <chrisrobin.soon@gmail.com>
2022-11-14 21:50:40 +0200

    fix: type in commands file

--- src/commands/main/help.ts
+++ src/commands/main/help.ts
@@ -1,4 +1,11 @@
-import { CommandInteraction, SlashCommandBuilder, APIEmbedField } from "discord.js";
+import {
+      CommandInteraction,
+      SlashCommandBuilder,
+      APIEmbedField,
+      ActionRowBuilder,
+      ButtonBuilder,
+      ButtonStyle,
+} from "discord.js";
 import { profilePic, linksField } from "../../components/const";
 import { embed } from "../../components/embeds";
 import { getCommands } from "../../components/commands";
@@ -18,6 +25,30 @@ module.exports = {
                         )),
       async execute(interaction: CommandInteraction) {
 
+            const buttons = new ActionRowBuilder<ButtonBuilder>()
+                  .addComponents(
+                        new ButtonBuilder()
+                              .setLabel("Invite me")
+                              .setURL("https://bit.ly/3wo2Tkh")
+                              .setStyle(ButtonStyle.Link),
+                        new ButtonBuilder()
+                              .setLabel("Vote")
+                              .setURL("https://top.gg/bot/719542118955090011/vote")
+                              .setStyle(ButtonStyle.Link),
+                        new ButtonBuilder()
+                              .setLabel("GitHub")
+                              .setURL("https://github.com/muumif/")
+                              .setStyle(ButtonStyle.Link),
+                        new ButtonBuilder()
+                              .setLabel("Terms Of Service")
+                              .setURL("https://github.com/muumif/Edgy-Loba/blob/master/TOS.md")
+                              .setStyle(ButtonStyle.Link),
+                        new ButtonBuilder()
+                              .setLabel("Privacy Policy")
+                              .setURL("https://github.com/muumif/Edgy-Loba/blob/master/PRIVACY.md")
+                              .setStyle(ButtonStyle.Link),
+                  );
+
             const chosenString = interaction.options.get("category")?.value;
             switch (chosenString) {
             case "help_misc": {
@@ -26,9 +57,8 @@ module.exports = {
                         .setThumbnail(profilePic(512))
                         .addFields(
                               getCommands("misc"),
-                              linksField("Links", false),
                         );
-                  return await interaction.editReply({ embeds: [helpEmbed] });
+                  return await interaction.editReply({ embeds: [helpEmbed], components: [buttons] });
             }
             case "help_stats": {
                   const helpEmbed = new embed().defaultEmbed()
@@ -36,9 +66,8 @@ module.exports = {
                         .setThumbnail(profilePic(512))
                         .addFields(
                               getCommands("stats"),
-                              linksField("Links", false),
                         );
-                  return await interaction.editReply({ embeds: [helpEmbed] });
+                  return await interaction.editReply({ embeds: [helpEmbed], components: [buttons] });
             }
             }
       },
