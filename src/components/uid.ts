030f19f83e52df4f5477eaa9494355f82087e8f5
muumi <chrisrobin.soon@gmail.com>
2023-02-06 21:08:11 +0200

    fix: rework uid converting

--- src/components/uid.ts
+++ src/components/uid.ts
@@ -6,36 +6,43 @@ import { logger } from "./logger";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const JSONBigInt = require("json-bigint")({ "storeAsString": true });
 
-export async function getUserUID(IGN: string, platform: "PC" | "X1" | "PS4", guildId: Snowflake | undefined, discordId: Snowflake) {
+export async function IGNToUID(IGN: string, platform: "PC" | "X1" | "PS4", guildId: Snowflake | undefined, discordId: Snowflake) {
       try {
-            const response = await axios.get(encodeURI(`${process.env.ALS_ENDPOINT}/nametouid?auth=${process.env.ALS_TOKEN}&player=${IGN}&platform=${platform}`), { transformResponse: [data => data] });
-            if (response.data.includes("Error")) { // This is used to check for Errors instead of catch. Could throw an error then handle it in catch.
-                  logger.error(response.data, { metadata: { serverId: guildId, discordId: discordId, IGN: IGN, platform: platform, file: filename(__filename) } });
-                  if (response.data.includes("Slow down !")) {
-                        return Promise.reject({ isGetUidError: true, message: "Try again in a few seconds!" });
+            let response = await makeRequest(`${process.env.ALS_ENDPOINT}/nametouid?auth=${process.env.ALS_TOKEN}&player=${IGN}&platform=${platform}`);
+
+            if (response.data.includes("Error")) {
+                  if (response.data.includes("Slow down!")) {
+                        await new Promise(resolve => setTimeout(resolve, 500));
+                        response = await makeRequest(`${process.env.ALS_ENDPOINT}/nametouid?auth=${process.env.ALS_TOKEN}&player=${IGN}&platform=${platform}`);
                   }
-                  return Promise.reject({ isGetUidError: true, message: response.data });
+                  else {
+                        throw new Error(response.data);
+                  }
             }
-            if (platform == "PC") { // PC players have to be separated because the data that is returned is different for them
-                  if (JSONBigInt.parse(response.data).uid == undefined) { // This is to check if an user was returned or not. Because the API doesn't throw an Error if no user exists.
-                        logger.error(response.data, { metadata: { serverId: guildId, discordId: discordId, IGN: IGN, platform: platform, file: filename(__filename) } });
-                        return Promise.reject({ isGetUidError: true, message: "Error: Unknown user! Please use Origin username!" });
-                  }
+
+            if (JSONBigInt.parse(response.data).uid == undefined || JSONBigInt.parse(response.data).result == undefined) {
+                  throw new Error("Unknown user! Please use Origin or console username!");
+            }
+
+            if (platform != "PC") {
                   logger.info("Fetched a users UID!", { metadata: { serverId: guildId, discordId: discordId, IGN: IGN, platform: platform, file: filename(__filename) } });
-                  return JSONBigInt.parse(response.data).uid;
+                  return JSONBigInt.parse(response.data).result;
             }
+
             logger.info("Fetched a users UID!", { metadata: { serverId: guildId, discordId: discordId, IGN: IGN, platform: platform, file: filename(__filename) } });
-            return JSONBigInt.parse(response.data).result;
+            return JSONBigInt.parse(response.data).uid;
+
       }
-      catch (error) {
-            // Can't use catch because the API doesn't throw any Errors even if there is one.
+      catch (error: any) {
+            logger.error(error, { metadata: { serverId: guildId, discordId: discordId, IGN: IGN, platform: platform, file: filename(__filename) } });
+            return Promise.reject(error);
       }
 
 }
 
 export async function UIDToIGN(UID: string, platform: "PC" | "X1" | "PS4", guildId: Snowflake, discordId: Snowflake) {
       try {
-            const data = await axios.get(encodeURI(`${process.env.ALS_ENDPOINT}/bridge?auth=${process.env.ALS_TOKEN}&uid=${UID}&platform=${platform}&skipRank=true`));
+            const data = await makeRequest(`${process.env.ALS_ENDPOINT}/bridge?auth=${process.env.ALS_TOKEN}&uid=${UID}&platform=${platform}&skipRank=true`);
             logger.info("ALS API fetched user!", { metadata:{ serverId: guildId, discordId: discordId, UID: UID, platform: platform, file: filename(__filename) } });
             return data.data.global.name;
       }
@@ -48,4 +55,9 @@ export async function UIDToIGN(UID: string, platform: "PC" | "X1" | "PS4", guild
                   logger.error(error, { metadata:{ serverId: guildId, discordId: discordId, UID: UID, platform: platform, file: filename(__filename) } });
             }
       }
+}
+
+async function makeRequest(URL: string) {
+      const data = await axios.get(encodeURI(URL), { transformResponse: [data => data] });
+      return data;
 }
