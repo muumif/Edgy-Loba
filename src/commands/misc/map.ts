8af1ae75637aa9ef06bc36dcef4549e0515b504c
muumif <chrisrobin.soon@gmail.com>
2023-03-23 16:05:25 +0200

    feat: if map images dosen't exist use not_found.jpg

--- src/commands/misc/map.ts
+++ src/commands/misc/map.ts
@@ -8,6 +8,7 @@ import { filename } from "../../components/const";
 import { logger } from "../../components/logger";
 import { cwd } from "process";
 import { scheduleJob } from "node-schedule";
+import { existsSync } from "fs";
 
 module.exports = {
       data: new SlashCommandBuilder()
@@ -25,9 +26,9 @@ module.exports = {
                   const canvas = createCanvas(canvasWidth, canvasHeight);
                   const context = canvas.getContext("2d");
 
-                  BRMap.src = await readFile(`${cwd()}/images/maps/${mapData.battle_royale.current.code}.jpg`);
-                  rankedBRMap.src = await readFile(`${cwd()}/images/maps/${mapData.ranked.current.code}.jpg`);
-                  LTMMap.src = await readFile(`${cwd()}/images/maps/${mapData.ltm.current.code}.jpg`);
+                  BRMap.src = existsSync(`${cwd()}/images/maps/${mapData.battle_royale.current.code}.jpg`) ? await readFile(`${cwd()}/images/maps/${mapData.battle_royale.current.code}.jpg`) : await readFile(`${cwd()}/images/maps/not_found.jpg`);
+                  rankedBRMap.src = existsSync(`${cwd()}/images/maps/${mapData.ranked.current.code}.jpg`) ? await readFile(`${cwd()}/images/maps/${mapData.ranked.current.code}.jpg`) : await readFile(`${cwd()}/images/maps/not_found.jpg`);
+                  LTMMap.src = existsSync(`${cwd()}/images/maps/${mapData.ltm.current.code}.jpg`) ? await readFile(`${cwd()}/images/maps/${mapData.ltm.current.code}.jpg`) : await readFile(`${cwd()}/images/maps/not_found.jpg`);
 
                   // Draw map images to the canvas
                   context.drawImage(BRMap, 0, 0, BRMap.width, BRMap.height, 0, 0, canvasWidth, canvasHeight);
@@ -103,39 +104,39 @@ module.exports = {
                                     .setTitle("Reminder BR");
                               reminderEmbed.setDescription(`The next map starts <t:${mapData.battle_royale.next.start}:R>.\nThe next map is **${mapData.battle_royale.next.map}**.\nCurrent map is **${mapData.battle_royale.current.map}**.`);
                               const nextDate = new Date(mapData.battle_royale.next.start * 1000);
-                              const job = scheduleJob(nextDate, async () => {
+                              scheduleJob(nextDate, async () => {
                                     await interaction.user.send(`**${mapData.battle_royale.next.map}** has started now!`);
-                                    await interaction.user.send({ embeds: [reminderEmbed] });
+                                    logger.info(`User [${interaction.user.username}] was reminded for the next BR map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
                               });
                               await interaction.user.send({ embeds: [reminderEmbed] });
                               await interaction.reply({ content: "You will be reminded when the next BR map starts!", ephemeral: true });
-                              logger.info(`User [${interaction.user.username}] (${interaction.user.id}) set a reminder for the next BR map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
+                              logger.info(`User [${interaction.user.username}] set a reminder for the next BR map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
                         }
                         if (interaction.customId == "remindNextRanked") {
                               const reminderEmbed = new embed().defaultEmbed()
                                     .setTitle("Reminder Ranked");
                               reminderEmbed.setDescription(`The next map starts <t:${mapData.ranked.next.start}:R>.\nThe next map is **${mapData.ranked.next.map}**.\nCurrent map is **${mapData.ranked.current.map}**.`);
                               const nextDate = new Date(mapData.ranked.next.start * 1000);
-                              const job = scheduleJob(nextDate, async () => {
+                              scheduleJob(nextDate, async () => {
                                     await interaction.user.send(`**${mapData.ranked.next.map}** has started now!`);
-                                    logger.info(`User [${interaction.user.username}] (${interaction.user.id}) was reminded for the next ranked map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
+                                    logger.info(`User [${interaction.user.username}] was reminded for the next ranked map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
                               });
                               await interaction.user.send({ embeds: [reminderEmbed] });
                               await interaction.reply({ content: "You will be reminded when the next ranked map starts!", ephemeral: true });
-                              logger.info(`User [${interaction.user.username}] (${interaction.user.id}) set a reminder for the next ranked map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
+                              logger.info(`User [${interaction.user.username}] set a reminder for the next ranked map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
                         }
                         if (interaction.customId == "remindNextLTM") {
                               const reminderEmbed = new embed().defaultEmbed()
                                     .setTitle("Reminder LTM");
                               reminderEmbed.setDescription(`The next map starts <t:${mapData.ltm.next.start}:R>.\nThe next map is **${mapData.ltm.next.map}**.\nCurrent map is **${mapData.ltm.current.map}**.`);
                               const nextDate = new Date(mapData.ltm.next.start * 1000);
-                              const job = scheduleJob(nextDate, async () => {
+                              scheduleJob(nextDate, async () => {
                                     await interaction.user.send(`**${mapData.ltm.next.map}** has started now!`);
-                                    logger.info(`User [${interaction.user.username}] (${interaction.user.id}) was reminded for the next LTM map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
+                                    logger.info(`User [${interaction.user.username}] was reminded for the next LTM map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
                               });
                               await interaction.user.send({ embeds: [reminderEmbed] });
                               await interaction.reply({ content: "You will be reminded when the next LTM map starts!", ephemeral: true });
-                              logger.info(`User [${interaction.user.username}] (${interaction.user.id}) set a reminder for the next LTM map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
+                              logger.info(`User [${interaction.user.username}] set a reminder for the next LTM map.`, { metadata: { discordId: interaction.user.id, serverId: interaction.guild?.id, file: filename(__filename) } });
                         }
                   });
             }
@@ -144,11 +145,10 @@ module.exports = {
                   console.log(error);
                   if (error.response) {
                         logger.error(error, { discordId: interaction.user.id, serverId: interaction.guildId, file: filename(__filename) });
-                        return await interaction.editReply({ embeds: [new embed().errorEmbed().setTitle("An error accrued!").setDescription(error.response.request.res.statusMessage.toString())] });
-                  }
-                  if (error) {
+                        await interaction.editReply({ embeds: [new embed().errorEmbed().setTitle("An error accrued!").setDescription(error.response.request.res.statusMessage.toString())] });
+                  } else {
                         logger.error(error, { discordId: interaction.user.id, serverId: interaction.guildId, file: filename(__filename) });
-                        return await interaction.editReply({ embeds: [new embed().errorEmbed().setTitle("Please try again in a few seconds!").setDescription(error.response.request.res.statusMessage.toString())] });
+                        await interaction.editReply({ embeds: [new embed().errorEmbed().setTitle("Please try again in a few seconds!").setDescription(error)] });
                   }
             }
 
